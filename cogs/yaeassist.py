c3a33e69ceef9419fd05ca338f7c902e3603d13a
CloudIsHere <90542854+cloudishere1@users.noreply.github.com>
2021-09-20 14:52:36 +0000

    new function, assists

---
+++ cogs/yaeassist.py
@@ -0,0 +1,77 @@
+from discord.ext import commands
+
+class Assist(commands.Cog):
+  
+  def __init__(self,bot):
+    self.bot = bot
+    
+
+  @commands.command(name="assist", help="Pings a helper role based on the channel, Cooldown = 10 minutes")
+  @commands.cooldown(1,600, commands.BucketType.member)
+  async def assist(self,ctx,*, reason = "unspecified"):
+      
+    if ctx.author == self.bot.user:
+      return
+
+    helper_roles = { "america" : 889517957904994314,
+                    "europe" : 889518004075913297,
+                    "asia" : 889517911994171422,
+                    "hk/tw/mo" : 889518084036132934}
+
+    helper_channel = { "ch_america" : 889514959514521621,
+                      "ch_europe" : 889514974920196106,
+                      "ch_asia" : 889486853617614869,
+                      "ch_hk/tw/mo" : 889515047682998282}     
+
+    wl_roles = { "1" : 889515455939756045,
+                "2" : 889515491847188602,
+                "3" : 889515522893438996,
+                "4" : 889515535283400765,
+                "5" : 889515551095934976,
+                "6" : 889515564291207239,
+                "7" : 889515581462695986,
+                "8" : 889515593982697592}
+
+    check = False
+    wl = "Unknown"
+
+    for items in list(wl_roles.values()):
+      if check == True:
+        break
+      else:  
+        for roles in ctx.author.roles:
+          if items == roles.id:
+            wl = [k for k, v in wl_roles.items() if v == items][0] #i dont understand this but this works
+            check = True
+            break
+       
+    if ctx.channel.id == helper_channel["ch_america"]:
+      await ctx.send(f'<@&{helper_roles["america"]}>\n {ctx.author.mention} Needs help for __{reason}__. WL = {wl}')
+
+    elif ctx.channel.id == helper_channel["ch_europe"]:
+      await ctx.send(f'<@&{helper_roles["europe"]}>\n {ctx.author.mention} Needs help for __{reason}__. WL = {wl}')
+
+    elif ctx.channel.id == helper_channel["ch_asia"]:
+      await ctx.send(f'<@&{helper_roles["asia"]}>\n {ctx.author.mention} Needs help for __{reason}__. WL = {wl}')
+
+    elif ctx.channel.id == helper_channel["ch_hk/tw/mo"]:
+      await ctx.send(f'<@&{helper_roles["hk/tw/mo"]}>\n {ctx.author.mention} Needs help for __{reason}__. WL = {wl}')
+
+    else:
+      await ctx.message.delete()
+      await ctx.send(f'{ctx.author.mention} Please use this command in <#889506367709274172>.', delete_after = 5)
+      self.assist.reset_cooldown(ctx)
+
+  @commands.command(hidden=True)
+  @commands.is_owner()
+  async def refresh(self,ctx):
+    self.assist.reset_cooldown(ctx)
+    
+  @assist.error
+  async def slowsend_error(self,ctx,error):
+    if isinstance(error,commands.CommandOnCooldown):
+      await ctx.send("**Command is still on cooldown**. Please try again after {:.0f} seconds".format(error.retry_after))
+
+
+def setup(bot):
+  bot.add_cog(Assist(bot))

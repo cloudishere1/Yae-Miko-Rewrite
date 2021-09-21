bdaca810f69a64d9c1e5aec723820316de6013d8
CloudIsHere <90542854+cloudishere1@users.noreply.github.com>
2021-09-21 15:24:25 +0000

    added stop and skip to cdban

--- cogs/yaemod.py
+++ cogs/yaemod.py
@@ -10,6 +10,9 @@ Permission: Ban member = True
   -> Bans a user, doesn't needed to be in the server
 -> cdban
   -> Bans a user after specified time(for fun)
+  -> wait for message 
+    ->yae stop = stops the ban prompt
+    ->yae skip = skips the ban countdown to zero
 """
 
 class Yaemod(commands.Cog):
@@ -45,31 +48,56 @@ class Yaemod(commands.Cog):
   @commands.command(name = "cdban", help = "bans a user after specified amount of time in seconds", aliases = ["countdownban", "timeban", "tb"], hidden = True)
   @commands.has_permissions(ban_members=True)
   async def cdban(self, ctx, user: commands.UserConverter = None, count: int = None,*,reason = None):
+    
+    await ctx.message.delete() #delete the command line
 
-    if user == None:
+    if user == None: #duh, you need a target to ban
         await ctx.send(f"Specify the person to ban `{self.bot.command_prefix}ban <@user> <reason>`")
         return
 
-    if ctx.author == user:
+    if ctx.author == user: #can't ban the user can you?
         await ctx.send("I cannot ban you.")
         return
 
     msg = await ctx.send(
-        f'Banning {user.mention} in {count}, Reason: {reason}')
+        f'Banning {user.mention} in {count} seconds, Reason: {reason}') #initial message
+    
+    def check(m): #checks for the user that invoked and channel it was invoked.
+      return m.author == ctx.author and m.channel == ctx.channel 
 
     while count:
-        await sleep(1)
+        try:
+          waitmsg = await self.bot.wait_for('message', check=check,timeout = 1)
+
+          if waitmsg.content.lower() == "yae stop": #stops the ban process
+            await ctx.send(f"I will not ban {user.mention} anymore.", delete_after = 5)
+            await msg.delete(delay = 5)
+            await waitmsg.delete(delay = 5)
+            return
+
+          elif waitmsg.content.lower() == "yae skip": #skips the countdown and ban immediately
+            count = 1
+            await waitmsg.delete(delay = 5)
+            await ctx.send("I skipped the countdown.",delete_after = 5)
+
+          else:
+            pass  #do nothing
+
+        except:
+          pass  #on timeout, do nothing
+          
         count -= 1
         await msg.edit(
-            content=f'Banning {user.mention} in {count}, Reason: {reason}')
-
+            content=f'Banning {user.mention} in {count} seconds, Reason: {reason}')
+            
     await sleep(1)
     
     try:
       await ctx.guild.ban(user, reason=f'{reason} :banned by {ctx.author}')
-      await msg.edit(content = f'Get out of here! <:YaeFeet:852664359696072734>')
+      await msg.edit(content = f'Get out of here! <:YaeFeet:889143402690662450>')
       await sleep(1)
-      await ctx.send(f'{user} has been banned, Reason: {reason}')
+      await msg.delete(delay = 10)
+      await ctx.send(f'{user} has been banned, Reason: {reason}', delete_after = 10)
 
     except discord.Forbidden:
       await ctx.send("Unable to ban an administrator")    
